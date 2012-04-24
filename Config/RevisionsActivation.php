603c8e5b19a7a65c9d7251c4273e46c297eaeb1f
Rachman Chavik <rchavik@xintesa.com>
2012-04-24 10:42:19 +0700

    various updates for 1.4

--- Config/RevisionsActivation.php
+++ Config/RevisionsActivation.php
@@ -1,9 +1,12 @@
 <?php
+
+App::uses('File', 'Utility');
+App::uses('ConnectionManager', 'Model');
+
 /**
  * Revisions Activation
  *
  * Activation class for Revisions plugin.
- * This is optional, and is required only if you want to perform tasks when your plugin is activated/deactivated.
  *
  * @package  Revisions
  * @author   David Wu <david@marksweep.com>
@@ -12,112 +15,112 @@
  * @copyright IPRO 2012
  */
 class RevisionsActivation {
+
 /**
  * onActivate will be called if this returns true
  *
  * @param  object $controller Controller
  * @return boolean
  */
-    public function beforeActivation(&$controller) {
-        return true;
-    }
+	public function beforeActivation(&$controller) {
+		return true;
+	}
+
 /**
  * Called after activating the plugin in ExtensionsPluginsController::admin_toggle()
  *
  * @param object $controller Controller
  * @return void
  */
-    public function onActivation(&$controller) {
-        // ACL: set ACOs with permissions
-        $controller->Croogo->addAco('Revisions'); // RevisionsController
-        $controller->Croogo->addAco('Revisions/admin_index'); // RevisionsController::admin_index()
-        $controller->Croogo->addAco('Revisions/view'); // RevisionsController::admin_index()
-        $controller->Croogo->addAco('Revisions/index', array('registered', 'public')); // RevisionsController::index()
+	public function onActivation(&$controller) {
+		// ACL: set ACOs with permissions
+		$controller->Croogo->addAco('Revisions'); // RevisionsController
+		$controller->Croogo->addAco('Revisions/admin_index'); // RevisionsController::admin_index()
+		$controller->Croogo->addAco('Revisions/view'); // RevisionsController::admin_index()
+		$controller->Croogo->addAco('Revisions/index', array('registered', 'public')); // RevisionsController::index()
 
-        // Main menu: add an Revisions link
-        //$mainMenu = $controller->Link->Menu->findByAlias('main');
-        //$controller->Link->Behaviors->attach('Tree', array(
-        //    'scope' => array(
-        //        'Link.menu_id' => $mainMenu['Menu']['id'],
-        //    ),
-        //));
-        //$controller->Link->save(array(
-        //    'menu_id' => $mainMenu['Menu']['id'],
-        //    'title' => 'Revisions',
-        //    'link' => 'plugin:Revisions/controller:Revisions/action:index',
-        //    'status' => 1,
-        //));
-        
-        // Attempt to add a revisions table to the database
-        $sql = file_get_contents(APP.'plugins'.DS.'revisions'.DS.'config'.DS.'revisions.sql');
-	        if(!empty($sql)){
-	        	App::import('Core', 'File');
-	        	App::import('Model', 'ConnectionManager');
-	        	$db = ConnectionManager::getDataSource('default');
+		// Main menu: add an Revisions link
+		//$mainMenu = $controller->Link->Menu->findByAlias('main');
+		//$controller->Link->Behaviors->attach('Tree', array(
+		//	'scope' => array(
+		//		'Link.menu_id' => $mainMenu['Menu']['id'],
+		//	),
+		//));
+		//$controller->Link->save(array(
+		//	'menu_id' => $mainMenu['Menu']['id'],
+		//	'title' => 'Revisions',
+		//	'link' => 'plugin:Revisions/controller:Revisions/action:index',
+		//	'status' => 1,
+		//));
 
-	        	$statements = explode(';', $sql);
+		// Attempt to add a revisions table to the database
+		$sql = file_get_contents(APP.'Plugin'.DS.'Revisions'.DS.'Config'.DS.'revisions.sql');
 
-		        foreach ($statements as $statement) {
-		            if (trim($statement) != '') {
-		                $db->query($statement);
-		            }
-		        }
-	        }
+		if(!empty($sql)){
+			$db = ConnectionManager::getDataSource('default');
+
+			$statements = explode(';', $sql);
+
+			foreach ($statements as $statement) {
+				if (trim($statement) != '') {
+					$db->query($statement);
+				}
+			}
+		}
 		return true;
-    }
+	}
+
 /**
  * onDeactivate will be called if this returns true
  *
  * @param  object $controller Controller
  * @return boolean
  */
-    public function beforeDeactivation(&$controller) {
-        return true;
-    }
+	public function beforeDeactivation(&$controller) {
+		return true;
+	}
+
 /**
  * Called after deactivating the plugin in ExtensionsPluginsController::admin_toggle()
  *
  * @param object $controller Controller
  * @return void
  */
-    public function onDeactivation(&$controller) {
-        // ACL: remove ACOs with permissions
-        $controller->Croogo->removeAco('Revisions'); // RevisionsController ACO and it's actions will be removed
+	public function onDeactivation(&$controller) {
+		// ACL: remove ACOs with permissions
+		$controller->Croogo->removeAco('Revisions'); // RevisionsController ACO and it's actions will be removed
 
-        // Main menu: delete Revisions link
-        //$link = $controller->Link->find('first', array(
-        //    'conditions' => array(
-        //         'Menu.alias' => 'main',
-        //         'Link.link' => 'plugin:Revisions/controller:Revisions/action:index',
-        //     ),
-        // ));
-        //$controller->Link->Behaviors->attach('Tree', array(
-        //    'scope' => array(
-        //        'Link.menu_id' => $link['Link']['menu_id'],
-        //    ),
-        //));
-        //if (isset($link['Link']['id'])) {
-        //    $controller->Link->delete($link['Link']['id']);
-        //}
-        
-        // Remove the database files
-        
-        // Attempt to add a revisions table to the database
-        $sql = file_get_contents(APP.'plugins'.DS.'revisions'.DS.'config'.DS.'revisions_deactivate.sql');
-	        if(!empty($sql)){
-	        	App::import('Core', 'File');
-	        	App::import('Model', 'ConnectionManager');
-	        	$db = ConnectionManager::getDataSource('default');
+		// Main menu: delete Revisions link
+		//$link = $controller->Link->find('first', array(
+		//	'conditions' => array(
+		//		 'Menu.alias' => 'main',
+		//		 'Link.link' => 'plugin:Revisions/controller:Revisions/action:index',
+		//	 ),
+		// ));
+		//$controller->Link->Behaviors->attach('Tree', array(
+		//	'scope' => array(
+		//		'Link.menu_id' => $link['Link']['menu_id'],
+		//	),
+		//));
+		//if (isset($link['Link']['id'])) {
+		//	$controller->Link->delete($link['Link']['id']);
+		//}
 
-	        	$statements = explode(';', $sql);
+		// Remove the database files
 
-		        foreach ($statements as $statement) {
-		            if (trim($statement) != '') {
-		                $db->query($statement);
-		            }
-		        }
-	        }
+		// Attempt to add a revisions table to the database
+		$sql = file_get_contents(APP.'Plugin'.DS.'Revisions'.DS.'Config'.DS.'revisions_deactivate.sql');
+		if(!empty($sql)){
+			$db = ConnectionManager::getDataSource('default');
 
-    }
+			$statements = explode(';', $sql);
+
+			foreach ($statements as $statement) {
+				if (trim($statement) != '') {
+					$db->query($statement);
+				}
+			}
+		}
+	}
+
 }
-?>
--- View/Helper/RevisionsHelper.php
+++ View/Helper/RevisionsHelper.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Revisions Helper
  *
@@ -12,124 +13,129 @@
  * @link     http://github.com/marksweep/revisions
  */
 class RevisionsHelper extends AppHelper {
+
 /**
  * Other helpers used by this helper
  *
  * @var array
  * @access public
  */
-    public $helpers = array(
-        'Html',
-        'Layout',
-    );
-    
-    public function __construct($settings = array()) {
-    	$this->viewVars = ClassRegistry::getObject('view')->viewVars;
-    }
+	public $helpers = array(
+		'Html',
+		'Layout',
+	);
+
 /**
  * Before render callback. Called before the view file is rendered.
  *
  * @return void
  */
-    public function beforeRender() {
-    }
+	public function beforeRender() {
+	}
+
 /**
  * After render callback. Called after the view file is rendered
  * but before the layout has been rendered.
  *
  * @return void
  */
-    public function afterRender() {
-    }
+	public function afterRender() {
+	}
+
 /**
  * Before layout callback. Called before the layout is rendered.
  *
  * @return void
  */
-    public function beforeLayout() {
-    }
+	public function beforeLayout() {
+	}
+
 /**
  * After layout callback. Called after the layout has rendered.
  *
  * @return void
  */
-    public function afterLayout() {
-    }
+	public function afterLayout() {
+	}
+
 /**
  * Called after LayoutHelper::setNode()
  *
  * @return void
  */
-    public function afterSetNode() {
-        // field values can be changed from hooks
-    }
+	public function afterSetNode() {
+		// field values can be changed from hooks
+	}
+
 /**
  * Called before LayoutHelper::nodeInfo()
  *
  * @return string
  */
-    public function beforeNodeInfo() {
-    	if (isset($this->viewVars['revision'])) {
-    		return '<div class="originalnodeinfo original" style="display:none">';
-    	}
-    }
+	public function beforeNodeInfo() {
+		if (isset($this->_View->viewVars['revision'])) {
+			return '<div class="originalnodeinfo original" style="display:none">';
+		}
+	}
+
 /**
  * Called after LayoutHelper::nodeInfo()
  *
  * @return string
  */
-    public function afterNodeInfo() {
-    	
-    	if (isset($this->viewVars['revision'])) {
-    		$body = '</div>';
-    	
-    		$body .= '<div class="revision revisionnodebody">' . $this->viewVars['revision']['Revision']['body'] . '</div>';
-    	
-    		return $body;
-    	}
-    	
-    	
-    }
+	public function afterNodeInfo() {
+		if (isset($this->_View->viewVars['revision'])) {
+			$body = '</div>';
+
+			$body .= '<div class="revision revisionnodebody">' . $this->viewVars['revision']['Revision']['body'] . '</div>';
+
+			return $body;
+		}
+	}
+
 /**
  * Called before LayoutHelper::nodeBody()
  *
  * @return string
  */
-    public function beforeNodeBody() {
-    	if (isset($this->viewVars['revision'])) {
-    		return '<div class="originalnode original">';
-    	}
-    }
+	public function beforeNodeBody() {
+		if (isset($this->_View->viewVars['revision'])) {
+			return '<div class="originalnode original">';
+		}
+	}
+
 /**
  * Called after LayoutHelper::nodeBody()
  *
  * @return string
  */
-    public function afterNodeBody() {
-    	if (isset($this->viewVars['revision'])) {
-    		 '</div>';
-    	}
-    }
+	public function afterNodeBody() {
+		if (isset($this->_View->viewVars['revision'])) {
+			 '</div>';
+		}
+	}
+
 /**
  * Called before LayoutHelper::nodeMoreInfo()
  *
  * @return string
  */
-    public function beforeNodeMoreInfo() {
-    	if (isset($this->viewVars['revision'])) {
-    		return '<div class="originalmoreinfo original">';
-    	}
-    }
+	public function beforeNodeMoreInfo() {
+		if (isset($this->_View->viewVars['revision'])) {
+			return '<div class="originalmoreinfo original">';
+		}
+	}
+
 /**
  * Called after LayoutHelper::nodeMoreInfo()
  *
  * @return string
  */
-    public function afterNodeMoreInfo() {
-    
-    	if (isset($this->viewVars['revision'])) {
-    		return '</div>';
-    	}
-    }
+	public function afterNodeMoreInfo() {
+
+		if (isset($this->_View->viewVars['revision'])) {
+			return '</div>';
+		}
+	}
+
 }
-?>
